0394cbe9495faeba57ce10611a7661c361e52990
Kyle Walker <KyleWalker@bisosoft.com>
2025-10-22 16:50:10 -0400

    Get, Post, Put, Delete

---
+++ Week_9/node_test/routes/users.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('User List');
+});
+
+router.post('/', (req, res) => {
+    res.send('Create User');
+});
+
+router.get('/new', (req, res) => {
+    res.render('users/new', { firstName: 'Test' });
+});
+
+router
+    .route('/:id')
+    .get((req, res) => {
+        console.log(req.user);
+        res.send(`Get User With ID: ${req.params.id}`);
+    })
+    .put((req, res) => {
+        res.send(`Update User With ID: ${req.params.id}`);
+    })
+    .delete((req, res) => {
+        res.send(`Delete User With ID: ${req.params.id}`);
+    });
+
+const users = [{ name: 'Joe' }, { name: 'Jane' }];
+
+router.param('id', (req, res, next, id) => {
+    req.user = users[id];
+    next();
+});
+
+module.exports = router;
--- Week_9/node_test/server.js
+++ Week_9/node_test/server.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
 
+// const logger = (req, res, next) => {
+//     console.log(req.originalUrl);
+//     next();
+// };
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+// app.use(logger);
+
 app.get('/', (req, res) => {
-    console.log('Here');
-    // res.send('Hello!');
-    // res.sendStatus(500);
-    // res.sendStatus(500).json({ message: 'Error' });
-    // res.json({ name: 'Joe', age: 19, class: 'sophmore' });
-    // res.download('server.js');
+    res.render('index', { text: 'Kyle' });
 });
 
+const userRouter = require('./routes/users');
+const router = require('./routes/users');
+
+app.use('/users', router);
+
 app.listen(3000);
+
+// app.get('/', (req, res) => {
+//     // console.log('Here');
+//     // res.send('Hello!');
+//     // res.sendStatus(500);
+//     // res.sendStatus(500).json({ message: 'Error' });
+//     // res.json({ name: 'Joe', age: 19, class: 'sophmore' });
+//     // res.download('server.js');
+// });
