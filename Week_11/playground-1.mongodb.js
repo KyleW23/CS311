4c8186e1daf35da8ba36f6d199331c33f87500b7
Kyle Walker <KyleWalker@bisosoft.com>
2025-10-27 16:43:30 -0400

    MongoDb Atlas, playground commands

---
+++ Week_11/playground-1.mongodb.js
@@ -0,0 +1,14 @@
+// use('school');
+// show("dbs"); || show dbs;
+// db.students.insertOne({
+//     name: 'Kyle',
+//     age: 21,
+//     gap: 4.0,
+// });
+// db.students.insertMany([
+//     { name: 'John', age: 32, gpa: 3.6 },
+//     { name: 'Joan', age: 19, gpa: 3.8 },
+//     { name: 'Jim', age: 28, gpa: 31 },
+// ]);
+// db.students.find();
+// db.students.find({ name: 'Kyle' });
