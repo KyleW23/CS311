ac57f3331c9b8731a2fdc7de174289786c49de8a
Kyle Walker <KyleWalker@bisosoft.com>
2025-11-03 16:48:15 -0500

    created search bar

---
+++ ReactWeatherApp/src/components/Weather.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './weather.css';
+import search_icon from '../assets/search.png';
+
+const Weather = () => {
+    return (
+        <div className='weather'>
+            <div className='search-bar'>
+                <input type='text' placeholder='Search' />
+                <img src={search_icon} alt='search icon' />
+            </div>
+        </div>
+    );
+};
+
+export default Weather;
